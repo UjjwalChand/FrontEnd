07a9859d9db60125f3acddb0aed990e4bd1568e3
lavinsh <rlavinsh@gmail.com>
2024-01-04 11:46:22 +0530

    Javascript notes and code updated

---
+++ JavaScript/code/Day-2/app.js
@@ -0,0 +1,44 @@
+// "use strict";
+// var a = "Lavinsh"
+// let b = 23;
+// const c = "geekster"
+
+// a = "gobinda";
+// console.log(a);
+//  var a = 23;
+//  var a= 56;
+
+//  let naam;
+//  naam = "ankit";
+//  console.log(naam);
+
+//  const age = 18;
+//  console.log(age);
+
+// 2nd
+
+{
+    // let a = "gobinda";
+    // a="ankit";
+    // console.log(a);
+    // console.log(a);
+
+}
+// let a;
+// a = "lavinsh";
+// console.log(a);
+
+{
+    // const a = "samarth";
+    // a="ujjwal";
+    // console.log(a);
+
+}
+// console.log(a);
+
+{
+    // var a = "avinash";
+}
+// a = "javascript"
+// console.log(a);
+
