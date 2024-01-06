1c451b358d5b940d9ca2f820c4a19957ce56032d
lavinsh <rlavinsh@gmail.com>
2024-01-06 10:27:47 +0530

    conditional statement

---
+++ JavaScript/code/Day-4/app.js
@@ -0,0 +1,82 @@
+// == VS ====
+
+// == That help you to check the value
+// === That help you to check the value and the type
+
+// let a=5;  // Number
+// let b = "5.0"; // String
+// console.log(typeof 5555555555555555556664444444n)
+// let c = 5.0;
+// console.log(a==b);
+
+// =======Ternary ==========
+// Syntax   (condition) ? true : false;
+let age = 19;
+// let ans = (age>=18) ? "you are eligible" : "you are not eligible";
+// console.log(ans);
+
+// Conditional statement
+// Syntax
+    //  if(age >= 18){
+    //     console.log("You are eligible");
+    //  }
+    //  console.log("you are not eligible");
+
+    // ======================If-Else=============
+    // if(age >= 18){
+    //     console.log("You are eligible");
+    //  }else{
+    //     console.log("you are not eligible");
+    //  }
+
+
+    // let a=5,b=6,c=7;
+    // if(a>b){
+    //     if(b<c){
+    //         console.log("C is the greatest");
+    //     }else{
+    //         console.log("A is the greatest");
+    //     }
+    // }else{
+    //     console.log("Better luck next time");
+    // }
+    let naam = "ankit";
+    if(naam=="lavinsh"){
+        console.log("Name is  matching");
+    }
+    else if(naam=="ankit"){
+       console.log("hello ankit");
+    }
+    else if(naam=="gobinda"){
+       console.log("hello gobinda");        
+    }else{
+     console.log("Better luck next time");
+    }
+
+    // switch(expression){
+    
+
+    // }
+
+    let num = 6;
+
+    switch(num){
+        case 1:
+            console.log("Sunday");
+            break;
+        case 2:
+            console.log("Monday");
+            break;
+        case 3: 
+        console.log("Tuesday");
+        break
+        case 4:
+            console.log("wednesday");
+            break;
+        default:
+            console.log("Not found");
+    }
+
+
+
+    
